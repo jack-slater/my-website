79322e949f938815e0b0fd8b9da6c7d220297de1
Slaytor1 <jackslater@hotmail.com>
2016-09-30 19:29:39 +0100

    added aboutus page and js effect to picture and text

--- js/main.js
+++ js/main.js
@@ -2,4 +2,19 @@ $(document).ready(function() {
 	$('h1').click(function() {
 		console.log('clicked');
 	});
+var spider = document.getElementById('spider');
+var beforeP = document.getElementById('before');
+var afterP = document.getElementById('after');
+
+var question = function() {
+	
+}
+
+var clickPic = function() {
+	spider.src = 'peterparker.png';
+	beforeP.style.visibility = 'hidden';
+	afterP.style.visibility = 'visible';
+	
+};
+spider.addEventListener('click',clickPic, false);
 });
